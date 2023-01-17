5a38964de1bf78d7d3d99dbddf57ba89b2ac92be
Tokzhan Beisekenova <tokzhan@longevityintime.org>
2023-01-17 17:35:44 +0800

    changed few files

---
+++ lib/Views/find_device_view.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import '../Widgets/scan_result_tile_widget.dart';
+import 'device_view.dart';
+
+class FindDevicesView extends StatelessWidget {
+  const FindDevicesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: key,
+      appBar: AppBar(
+        title: const Text('Find Devices'),
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.black,
+            ),
+            onPressed: Platform.isAndroid
+                ? () => FlutterBluePlus.instance.turnOff()
+                : null,
+            child: const Text('TURN OFF'),
+          ),
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => FlutterBluePlus.instance
+            .startScan(timeout: const Duration(seconds: 4)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              StreamBuilder<List<BluetoothDevice>>(
+                stream: Stream.periodic(const Duration(seconds: 2))
+                    .asyncMap((_) => FlutterBluePlus.instance.connectedDevices),
+                initialData: const [],
+                builder: (c, snapshot) => Column(
+                  children: snapshot.data!
+                      .map((d) => ListTile(
+                            title: Text('Your device  ${d.name}'),
+                            subtitle: Text('Your device ${d.id.toString()}'),
+                            trailing: StreamBuilder<BluetoothDeviceState>(
+                              stream: d.state,
+                              initialData: BluetoothDeviceState.disconnected,
+                              builder: (c, snapshot) {
+                                print('Controller Stream ${d.state}');
+                                if (snapshot.data ==
+                                    BluetoothDeviceState.connected) {
+                                  return ElevatedButton(
+                                    child: const Text('OPEN'),
+                                    onPressed: () => Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                DeviceView(device: d))),
+                                  );
+                                }
+                              
+                                print('333333333333333333333333333333333333 ${snapshot.data.toString()}');
+                                return Text(snapshot.data.toString());
+                              },
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+              StreamBuilder<List<ScanResult>>(
+                stream: FlutterBluePlus.instance.scanResults,
+                initialData: const [],
+                builder: (c, snapshot) => Column(
+                  children: snapshot.data!
+                      .map(
+                        (r) => ScanResultTileWidget(
+                          result: r,
+                          onTap: () => Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            r.device.connect();
+                            return DeviceView(device: r.device);
+                          })),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: FlutterBluePlus.instance.isScanning,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data!) {
+            return FloatingActionButton(
+              onPressed: () => FlutterBluePlus.instance.stopScan(),
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.stop),
+            );
+          } else {
+            return FloatingActionButton(
+                child: const Icon(Icons.search),
+                onPressed: () => FlutterBluePlus.instance
+                    .startScan(timeout: const Duration(seconds: 4)));
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/Views/flutter_blu_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import 'bluetooth_off_view.dart';
+import 'find_device_view.dart';
+
+class FlutterBlueApp extends StatelessWidget {
+  const FlutterBlueApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.lightBlue,
+      home: StreamBuilder<BluetoothState>(
+          stream: FlutterBluePlus.instance.state,
+          initialData: BluetoothState.unknown,
+          builder: (c, snapshot) {
+            final state = snapshot.data;
+            print('Please select your devise: $state');
+            if (state == BluetoothState.on) {
+              return const FindDevicesView();
+            }
+            return BluetoothOffView(state: state);
+          }),
+    );
+  }
+}
